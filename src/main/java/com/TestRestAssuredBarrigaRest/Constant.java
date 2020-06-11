29a84dbe2c0bd87b78704f77a7ff49211da259dc
Pablo <pablo.mg@hotmail.com>
2020-06-11 20:48:33 -0300

    init test api barriga with rest assured

---
+++ src/main/java/com/TestRestAssuredBarrigaRest/Constant.java
@@ -0,0 +1,14 @@
+package com.TestRestAssuredBarrigaRest;
+
+import io.restassured.http.ContentType;
+
+public interface Constant {
+
+    String APP_BASE_URL = "https://barrigarest.wcaquino.me";
+    Integer APP_PORT = 443; // http -> 80
+    String APP_BASE_PATH = "";
+
+    ContentType APP_CONTENT_TYPE = ContentType.JSON;
+
+    Long MAX_TIMEOUT = 1000L;
+}
