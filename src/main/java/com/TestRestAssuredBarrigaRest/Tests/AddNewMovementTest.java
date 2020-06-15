06a5c4259d3ff7af2fa793547bcaf74c34974322
Pablo <pablo.mg@hotmail.com>
2020-06-14 22:12:32 -0300

    Added the test "ValidateRequiredsFields"

--- src/main/java/com/TestRestAssuredBarrigaRest/Tests/AddNewMovementTest.java
+++ src/main/java/com/TestRestAssuredBarrigaRest/Tests/AddNewMovementTest.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class AddNewMovementTest extends BaseTest {
 
@@ -24,5 +25,23 @@ public class AddNewMovementTest extends BaseTest {
         given().header("Authorization", "JWT " + TOKEN).body(movement)
                 .when().post("/transacoes")
                 .then().statusCode(201);
+    }
+
+    @Test
+    public void validateRequiredsFields(){
+        given().header("Authorization", "JWT " + TOKEN).body("{}")
+        .when().post("/transacoes")
+        .then()
+            .statusCode(400)
+            .body("$", hasSize(8))
+            .body("msg",hasItems(
+                            "Data da Movimentação é obrigatório",
+                            "Data do pagamento é obrigatório",
+                            "Descrição é obrigatório",
+                            "Interessado é obrigatório",
+                            "Valor é obrigatório",
+                            "Valor deve ser um número",
+                            "Conta é obrigatório",
+                            "Situação é obrigatório"));
     }
 }
