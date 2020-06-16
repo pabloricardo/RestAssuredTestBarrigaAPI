9a99f46e9631b375cf982ef350433fea5a1cf5ad
Pablo <pablo.mg@hotmail.com>
2020-06-15 23:57:59 -0300

    suit test

--- src/main/java/com/TestRestAssuredBarrigaRest/suitTest/SuitAllTests.java
+++ src/main/java/com/TestRestAssuredBarrigaRest/suitTest/SuitAllTests.java
@@ -1,4 +1,17 @@
 package com.TestRestAssuredBarrigaRest.suitTest;
 
-public class SuitAllTests {
+
+import com.TestRestAssuredBarrigaRest.Core.BaseTest;
+import com.TestRestAssuredBarrigaRest.Tests.AccountTest;
+import com.TestRestAssuredBarrigaRest.Tests.DoNotAccessWithoutTokenTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(org.junit.runners.Suite.class)
+@SuiteClasses({
+        DoNotAccessWithoutTokenTest.class,
+        AccountTest.class
+})
+public class SuitAllTests extends BaseTest {
+
 }
